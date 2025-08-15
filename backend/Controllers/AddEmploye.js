fb5f208efa87f8737e153b8423234a6988d58f6d
varsha-2530 <vb8607518@gmaail.com>
2025-08-15 13:40:50 +0530

    backend-update

--- backend/Controllers/AddEmploye.js
+++ backend/Controllers/AddEmploye.js
@@ -4,25 +4,28 @@ export const AddEmploye = async (req, res) => {
   try {
     const { phone } = req.body;
 
+    
     const existingEmployee = await Employee.findOne({ phone });
     if (existingEmployee) {
       return res.status(400).json({
-        message: "This phone number is already registered.",
+        success: false,
+        message: "This phone number is already registered."
       });
     }
 
     const employee = new Employee(req.body);
-    console.log(employee);
-
     await employee.save();
-    res.status(200).json({
+
+    res.status(201).json({
+      success: true,
       message: "Employee added successfully.",
       employee
     });
   } catch (error) {
-    res.status(400).json({
-      message: "Not Add Successfully",
-      error,
+    res.status(500).json({
+      success: false,
+      message: "Error adding employee.",
+      error: error.message
     });
   }
 };
